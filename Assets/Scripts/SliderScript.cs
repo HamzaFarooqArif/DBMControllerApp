5da387c21410292eebdfb2b59e39d1bcfc6841b7
Hamza <m.hamza.arif786@gmail.com>
2019-10-05 01:03:12 +0500

    ColorPickerDone

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -6,11 +6,46 @@ using UnityEngine.UI;
 public class SliderScript : MonoBehaviour
 {
     private InterfaceDBM interfaceDBM = null;
-    private int sliderValue;
+    private Slider slider;
     // Start is called before the first frame update
     void Start()
     {
         interfaceDBM = (InterfaceDBM)GameObject.Find("Driver").GetComponent(typeof(InterfaceDBM));
+        slider = this.gameObject.GetComponent<Slider>();
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+    public void ValueChangeCheck()
+    {
+        if (this.transform.parent.parent.name.Equals("ColorPickerUpper"))
+        {
+            if (this.transform.name.Equals("HueSlider"))
+            {
+                interfaceDBM.setColorSpace(1, hue: (int)slider.value);
+            }
+            else if (this.transform.name.Equals("SaturationSlider"))
+            {
+                interfaceDBM.setColorSpace(1, sat: (int)slider.value);
+            }
+            else if (this.transform.name.Equals("ValueSlider"))
+            {
+                interfaceDBM.setColorSpace(1, val: (int)slider.value);
+            }
+        }
+        else if (this.transform.parent.parent.name.Equals("ColorPickerLower"))
+        {
+            if (this.transform.name.Equals("HueSlider"))
+            {
+                interfaceDBM.setColorSpace(0, hue: (int)slider.value);
+            }
+            else if (this.transform.name.Equals("SaturationSlider"))
+            {
+                interfaceDBM.setColorSpace(0, sat: (int)slider.value);
+            }
+            else if (this.transform.name.Equals("ValueSlider"))
+            {
+                interfaceDBM.setColorSpace(0, val: (int)slider.value);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/TextBoxScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxScript : MonoBehaviour
+{
+    private InterfaceDBM interfaceDBM = null;
+    private InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interfaceDBM = (InterfaceDBM)GameObject.Find("Driver").GetComponent(typeof(InterfaceDBM));
+        inputField = this.gameObject.GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.parent.parent.name.Equals("ColorPickerUpper"))
+        {
+            List<float> clr = interfaceDBM.getColor(1);
+            string hueVal = clr[0].ToString();
+            string satVal = clr[1].ToString();
+            string valVal = clr[2].ToString();
+            if (this.transform.name.Equals("HueValue"))
+            {
+                inputField.text = hueVal;
+            }
+            else if (this.transform.name.Equals("SaturationValue"))
+            {
+                inputField.text = satVal;
+            }
+            else if (this.transform.name.Equals("LightnessValue"))
+            {
+                inputField.text = valVal;
+            }
+        }
+        else if (this.transform.parent.parent.name.Equals("ColorPickerLower"))
+        {
+            List<float> clr = interfaceDBM.getColor(0);
+            string hueVal = clr[0].ToString();
+            string satVal = clr[1].ToString();
+            string valVal = clr[2].ToString();
+            if (this.transform.name.Equals("HueValue"))
+            {
+                inputField.text = hueVal;
+            }
+            else if (this.transform.name.Equals("SaturationValue"))
+            {
+                inputField.text = satVal;
+            }
+            else if (this.transform.name.Equals("LightnessValue"))
+            {
+                inputField.text = valVal;
+            }
+        }
+    }
+}
